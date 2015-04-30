2b31cad29f963aa6464260a4d8e013dbd7f5e3b5
Bob den Otter <bobdenotter@gmail.com>
2015-04-30 14:21:56 +0200

    Opruimen

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -1,38 +1,41 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  var app = angular.module('application', [
-    'ui.router',
-    'ngAnimate',
+    var app = angular.module('application', [
+        'ui.router',
+        'ngAnimate',
 
-    //foundation
-    'foundation',
-    'foundation.dynamicRouting',
-    'foundation.dynamicRouting.animations'
-  ])
-    .config(config)
-    .run(run)
-  ;
+        //foundation
+        'foundation',
+        'foundation.dynamicRouting',
+        'foundation.dynamicRouting.animations'
+    ])
+        .config(config)
+        .run(run)
+    ;
 
-  config.$inject = ['$urlRouterProvider', '$locationProvider'];
+    config.$inject = ['$urlRouterProvider', '$locationProvider'];
 
-  function config($urlProvider, $locationProvider) {
-    $urlProvider.otherwise('/');
+    function config($urlProvider, $locationProvider) {
+        $urlProvider.otherwise('/');
 
-    $locationProvider.html5Mode({
-      enabled: true,
-      requireBase: false
-    });
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+        });
 
-    $locationProvider.hashPrefix('!');
-  }
+        $locationProvider.hashPrefix('!');
+    }
 
-  function run() {
-    FastClick.attach(document.body);
-  }
+    function run() {
+        FastClick.attach(document.body);
+    }
 
-  //
-  app.filter('html', function($sce) { return $sce.trustAsHtml; });
+    //
+    app.filter('html', function($sce) {
+        return $sce.trustAsHtml; }
+    );
+
 
 })();
 
--- client/assets/js/entries.js
+++ client/assets/js/entries.js
@@ -1,32 +1,37 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  angular.module('application').controller('EntriesController',
-    [ '$http', '$scope', '$stateParams', '$controller',
-    function ($http, $scope, $stateParams, $controller)
-    {
+    angular.module('application').controller('EntriesController',
+        [
+            '$http', '$scope', '$stateParams', '$controller',
+            function ($http, $scope, $stateParams, $controller)
+            {
 
-    angular.extend(this, $controller(
-        'DefaultController',
-        { $http: $http, $scope: $scope, $stateParams: $stateParams}
-        ));
+                angular.extend(this, $controller(
+                    'DefaultController',
+                    { $http: $http, $scope: $scope, $stateParams: $stateParams}
+                    ));
 
-    $scope.loadData = function() {
-      $http
-          .get("http://bob.biz.tm/json/entries")
-          .then(function(result){
-            $scope.entries = result.data.entries;
-          }).catch(function(e) {
-            console.log(e);
-          });
+                $scope.loadEntries = function() {
+                    $http
+                    .get("http://bob.biz.tm/json/entries")
+                    .then(function(result){
+                        $scope.entries = result.data.entries;
+                    }).catch(function(e) {
+                        console.log("Could not fetch Entries");
+                        console.log(e);
+                    });
 
-        };
+                };
 
-    if (!$scope.entries) {
-      $scope.loadData();
-    }
 
-  }]);
+                if (!$scope.entries) {
+                    $scope.loadEntries();
+                }
+
+            }
+        ]
+    );
 
 
 
