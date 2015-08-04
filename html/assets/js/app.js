31c459402f7446f51a79bb7747881b76962c9b9e
Bob den Otter <bob@twokings.nl>
2015-08-04 20:11:34 +0200

    Adding HTML

--- html/assets/js/app.js
+++ html/assets/js/app.js
@@ -141,71 +141,6 @@
 
   var itgwo = angular.module('itgwo');
 
-  // Source: http://stackoverflow.com/questions/23804981/alternative-of-httpprovider-responseinterceptors
-  // See also: https://docs.angularjs.org/api/ng/service/$http#interceptors
-  // Not entirely sure if this is the best solution!
-
-  itgwo.factory('itgwo.service.httpInterceptor',
-    ['$q', '$window',
-    function ($q, $window) {
-
-      var count = 0;
-
-      return {
-        'request': function(config) {
-          count++;
-          // console.log('start', count);
-          return config;
-        },
-        // 'requestError': function(rejection) {
-        //   return $q.reject(rejection);
-        // },
-       'response': function(response) {
-          count--;
-          // console.log('stop', count);
-          return response;
-        },
-       'responseError': function(rejection) {
-          count--;
-          // console.log('stop', count);
-          return $q.reject(rejection);
-        },
-        'isLoading': function() {
-          return count != 0;
-        }
-      };
-      // end: return
-
-    }
-  ]);
-})();
-
-(function() {
-  'use strict';
-
-  var itgwo = angular.module('itgwo');
-
-  itgwo.service('itgwo.service.notification', [
-    'config', '$filter', '$http', 'FoundationApi',
-    function (config, $filter, $http, FoundationApi) {
-
-      this.notification = function(e) {
-          FoundationApi.publish('notifications', {
-            title: 'Error: ' + e.errors.title,
-            content: e.errors.detail,
-            color: 'alert'
-          });
-      }
-
-  }]);
-
-})();
-
-(function() {
-  'use strict';
-
-  var itgwo = angular.module('itgwo');
-
   itgwo.controller('itgwo.controller.anekdotes', [
     '$scope', '$controller', '$state','$http', '$rootScope', 'config', 'itgwo.service.notification',
     function ($scope, $controller, $state, $http, $rootScope, config, itgwoServiceNotification) {
@@ -425,5 +360,70 @@
 
     }
   ]);
+
+})();
+
+(function() {
+  'use strict';
+
+  var itgwo = angular.module('itgwo');
+
+  // Source: http://stackoverflow.com/questions/23804981/alternative-of-httpprovider-responseinterceptors
+  // See also: https://docs.angularjs.org/api/ng/service/$http#interceptors
+  // Not entirely sure if this is the best solution!
+
+  itgwo.factory('itgwo.service.httpInterceptor',
+    ['$q', '$window',
+    function ($q, $window) {
+
+      var count = 0;
+
+      return {
+        'request': function(config) {
+          count++;
+          // console.log('start', count);
+          return config;
+        },
+        // 'requestError': function(rejection) {
+        //   return $q.reject(rejection);
+        // },
+       'response': function(response) {
+          count--;
+          // console.log('stop', count);
+          return response;
+        },
+       'responseError': function(rejection) {
+          count--;
+          // console.log('stop', count);
+          return $q.reject(rejection);
+        },
+        'isLoading': function() {
+          return count != 0;
+        }
+      };
+      // end: return
+
+    }
+  ]);
+})();
+
+(function() {
+  'use strict';
+
+  var itgwo = angular.module('itgwo');
+
+  itgwo.service('itgwo.service.notification', [
+    'config', '$filter', '$http', 'FoundationApi',
+    function (config, $filter, $http, FoundationApi) {
+
+      this.notification = function(e) {
+          FoundationApi.publish('notifications', {
+            title: 'Error: ' + e.errors.title,
+            content: e.errors.detail,
+            color: 'alert'
+          });
+      }
+
+  }]);
 
 })();
